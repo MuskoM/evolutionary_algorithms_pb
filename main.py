4e18932ad4b9cd2a26ec502c5f910bad22e32ba8
MuskoM <git@musko.codes>
2023-03-29 16:07:17 +0200

    Use one var for testing function

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import mutation_functions as mut
 from visualization import Visualize
 
 if __name__ == '__main__':
+
+    TESTING_FUNCTION = test.levy
+
     population = 6
     algorithm = de.DifferentialEvolution(
         population=12,
         iterations=100,
-        test_func=test.rosenbrock ,
+        test_func=TESTING_FUNCTION,
         mutation_func=mut.de_best_2
     )
 
@@ -16,5 +19,6 @@ if __name__ == '__main__':
     print("Best output:")
     print(f'Value: {algorithm.best}')
     print(f'Value: {algorithm.best_vector}')
-    vis = Visualize(test.ackley)
+
+    vis = Visualize(TESTING_FUNCTION)
     vis.show_best([*algorithm.best_vector,algorithm.best])
